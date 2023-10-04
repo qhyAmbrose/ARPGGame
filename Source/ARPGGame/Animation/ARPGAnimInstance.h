b1799b7699abb974b2859bdc9a8d6eb47ad6ce27
qhy <724769318@qq.com>
2023-10-04 22:14:30 +0800

    Lyra框架0.01

---
+++ Source/ARPGGame/Animation/ARPGAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ARPGAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPGGAME_API UARPGAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
